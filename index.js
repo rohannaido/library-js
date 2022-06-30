b20a0f53eed7bcbb490efe897de0fd8064e648ef
rohannaido <87189343+rohannaido@users.noreply.github.com>
2022-06-30 20:18:41 +0530

    feat: Toggle Read and Unread

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-let myLibrary = JSON.parse(localStorage.getItem("LIBRARY_BOOKS"));
+let myLibrary = [];
+
+if(localStorage.getItem("LIBRARY_BOOKS")){
+    myLibrary = JSON.parse(localStorage.getItem("LIBRARY_BOOKS"));
+}
 
 const titleInput = document.querySelector(".title-input");
 const authorInput = document.querySelector(".author-input");
@@ -6,6 +10,7 @@ const pagesInput = document.querySelector(".pagecount-input");
 const booksTable = document.querySelector(".books-table-div table tbody");
 const readInput = document.querySelector(".new-book-info input[type='radio']");
 
+let key = 0;
 const newBookBtn = document.querySelector(".new-book-submit")
 
 displayLibrary();
@@ -18,6 +23,7 @@ function saveLibrary(){
     localStorage.setItem("LIBRARY_BOOKS", stringList);
 }
 
+//Book constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -25,6 +31,7 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+//Add single book to library
 function addBookToLibrary(e){
     e.preventDefault();
     let newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.checked);
@@ -33,30 +40,56 @@ function addBookToLibrary(e){
     saveLibrary();
 }
 
+//Display all the books
 function displayLibrary() {
-    for(const book of myLibrary){
-        addBookToTable(book);
+    for(let i = 0; i < myLibrary.length; i++){
+        addBookToTable(myLibrary[i]);
     }
 }
 
+//Add book to table element in HTML
 function addBookToTable(book){
+    console.log(key);
     let newRow = document.createElement("tr");
-
+    
     let newTitleCell = document.createElement("td");
     let newAuthorCell = document.createElement("td");
     let newPagesCell = document.createElement("td");
     let newReadCell = document.createElement("td");
-    
+    let newReadButton = document.createElement("button");
+
+    let newDeleteCell = document.createElement("td");
+    let newDeleteButton = document.createElement("button");
+
     newTitleCell.innerText = book.title;
     newAuthorCell.innerText = book.author;
     newPagesCell.innerText = book.pages;
-    newReadCell.innerText = book.read;
+
+    newReadButton.innerText = book.read ? "Read" : "Unread";
+    newReadButton.classList = "book-read-unread-button";
+    newReadButton.setAttribute("data-key", key);
+    newReadCell.appendChild(newReadButton);
+    newReadButton.addEventListener("click", toggleReadUnread);
+
+    newDeleteButton.innerText = "Delete";
+    newDeleteButton.classList = "book-delete-button";
+    newDeleteButton.setAttribute("data-key", key);
+    newDeleteCell.appendChild(newDeleteButton);
     
     newRow.appendChild(newTitleCell);
     newRow.appendChild(newAuthorCell);
     newRow.appendChild(newPagesCell);
     newRow.appendChild(newReadCell);
-
+    newRow.appendChild(newDeleteCell);
+    
     booksTable.appendChild(newRow);
+    key++;
 }
 
+//Toggle Read Unread function
+function toggleReadUnread(e){
+    let index = (e.target.getAttribute("data-key"));
+    myLibrary[index].read = !myLibrary[index].read;
+    e.target.innerText = myLibrary[index].read ? "Read" : "Unread";
+    saveLibrary();
+}
