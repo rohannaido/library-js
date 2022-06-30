aa6b103b4b4df1d25b4d77985aaa29517153f856
rohannaido <87189343+rohannaido@users.noreply.github.com>
2022-06-30 19:44:58 +0530

    feat: localstorage implementation

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-let myLibrary = [];
-myLibrary.push(new Book("asdf","aasdsdf",24355, true));
+let myLibrary = JSON.parse(localStorage.getItem("LIBRARY_BOOKS"));
+
 const titleInput = document.querySelector(".title-input");
 const authorInput = document.querySelector(".author-input");
 const pagesInput = document.querySelector(".pagecount-input");
-const booksTable = document.querySelector(".books-table-div table");
+const booksTable = document.querySelector(".books-table-div table tbody");
+const readInput = document.querySelector(".new-book-info input[type='radio']");
 
 const newBookBtn = document.querySelector(".new-book-submit")
 
 displayLibrary();
+
+newBookBtn.addEventListener("click", addBookToLibrary)
+
+
+function saveLibrary(){
+    let stringList = JSON.stringify(myLibrary);
+    localStorage.setItem("LIBRARY_BOOKS", stringList);
+}
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -18,36 +27,36 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(e){
     e.preventDefault();
-    let title = titleInput.value;
-    console.log(title + " TITLE")
-    myLibrary.push(new Book("asdf","asdfaadsf",234223, true));
-    displayLibrary();
+    let newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.checked);
+    myLibrary.push(newBook);
+    addBookToTable(newBook);
+    saveLibrary();
 }
 
 function displayLibrary() {
-    
     for(const book of myLibrary){
-        console.log(book);
-        let newRow = document.createElement("tr");
-
-        let newTitleCell = document.createElement("td");
-        let newAuthorCell = document.createElement("td");
-        let newPagesCell = document.createElement("td");
-        let newReadCell = document.createElement("td");
-        
-        newTitleCell.innerText = book.title;
-        newAuthorCell.innerText = book.author;
-        newPagesCell.innerText = book.pages;
-        newReadCell.innerText = book.read;
-        
-        newRow.appendChild(newTitleCell);
-        newRow.appendChild(newAuthorCell);
-        newRow.appendChild(newPagesCell);
-        newRow.appendChild(newReadCell);
-
-        booksTable.appendChild(newRow);
+        addBookToTable(book);
     }
-    // booksTable.
 }
 
-newBookBtn.addEventListener("click", addBookToLibrary)
+function addBookToTable(book){
+    let newRow = document.createElement("tr");
+
+    let newTitleCell = document.createElement("td");
+    let newAuthorCell = document.createElement("td");
+    let newPagesCell = document.createElement("td");
+    let newReadCell = document.createElement("td");
+    
+    newTitleCell.innerText = book.title;
+    newAuthorCell.innerText = book.author;
+    newPagesCell.innerText = book.pages;
+    newReadCell.innerText = book.read;
+    
+    newRow.appendChild(newTitleCell);
+    newRow.appendChild(newAuthorCell);
+    newRow.appendChild(newPagesCell);
+    newRow.appendChild(newReadCell);
+
+    booksTable.appendChild(newRow);
+}
+
